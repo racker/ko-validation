61c8e6b24ab707f148cd1175d2100a081ed4ae6e
Luiz Fernando Ribeiro <luizfar@gmail.com>
2013-12-06 19:32:00 -0200

    Add length validator.

---
+++ spec/validators/length-validator-spec.js
@@ -0,0 +1,33 @@
+describe('ko.validators.lengthValidator', function() {
+  var validator, result;
+
+  beforeEach(function() {
+    validator = ko.validators.lengthValidator('Field', 5);
+  });
+
+  it('should pass if the length is in the limit', function() {
+    result = validator.validate('foo');
+    expect(result.isValid).toBe(true);
+  });
+
+  it('should check the length of an integer', function() {
+    result = validator.validate(123);
+    expect(result.isValid).toBe(true);
+  });
+
+  it('should have empty message on valid value', function() {
+    result = validator.validate('foo');
+    expect(result.message).toBe('');
+  });
+
+  it('should fail if the length is not in the limit', function() {
+    result = validator.validate('foo-bar');
+    expect(result.isValid).toBe(false);
+  });
+
+  it('should have correct message on failure', function() {
+    result = validator.validate('foo-bar');
+    expect(result.message).toBe('Field cannot be longer than 5 characters.');
+  });
+});
+
---
+++ src/validators/length-validator.js
@@ -0,0 +1,18 @@
+ko.validators.lengthValidator = function (fieldName, length) {
+  return {
+    validate: function (value) {
+      var result = {};
+
+      result.isValid = value.toString().trim().length <= length;
+
+      result.message = result.isValid ? '' : ko.validation.config.i18n(
+       '{$field} cannot be longer than {$len} characters.',
+        {'field' : fieldName, 'len' : length}
+      );
+
+      return result;
+    }
+  };
+};
+
+
