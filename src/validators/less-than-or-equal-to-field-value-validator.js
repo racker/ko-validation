61fe52c1cdd856eb8d9cfa6904c1842bc7aac240
Luiz Fernando Ribeiro <luizfar@gmail.com>
2013-12-09 14:20:13 -0200

    Extract dependentFieldNumericValidator.

--- src/validators/less-than-or-equal-to-field-value-validator.js
+++ src/validators/less-than-or-equal-to-field-value-validator.js
@@ -1,28 +1,20 @@
 ko.validators.lessThanOrEqualToFieldValueValidator = function (fieldName, otherFieldName, otherFieldId) {
+  var dependentFieldNumericValidator;
+
+  dependentFieldNumericValidator = ko.validators.dependentFieldNumericValidator(
+    otherFieldId,
+    ko.validators.utilities.buildString(
+      '{$field} must be less than or equal to {$otherField}',
+      { 'field': fieldName, 'otherField': otherFieldName }
+    ),
+    function (value, otherFieldValue) {
+      return value <= otherFieldValue;
+    }
+  );
+
   return {
     validate: function (value) {
-      var utils, otherField, otherFieldValue, valueNumeric, otherFieldValueNumeric, result;
-
-      utils = ko.validators.utilities;
-
-      otherField = document.getElementById(otherFieldId);
-      otherFieldValue = otherField.value;
-
-      if (!utils.isInteger(otherFieldValue)) {
-        return { isValid: true, message: '' };
-      }
-
-      valueNumeric = parseInt(value, 10);
-      otherFieldValueNumeric = parseInt(otherFieldValue, 10);
-
-      result = {};
-      result.isValid = utils.isInteger(value) && valueNumeric <= otherFieldValueNumeric;
-      result.message = result.isValid ? '' : utils.buildString(
-        '{$field} must be less than or equal to {$otherField}',
-        { 'field': fieldName, 'otherField': otherFieldName }
-      );
-
-      return result;
+      return dependentFieldNumericValidator.validate(value);
     }
   };
 };
