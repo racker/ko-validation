70fc1b17938904c4c3f7782640034b286859a6b1
Luiz Fernando Ribeiro <luizfar@gmail.com>
2013-12-09 15:29:10 -0200

    Add minification.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,27 @@ module.exports = function(grunt) {
         browsers: ['PhantomJS']
       }
     },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/ko-validation.js'
+      }
+    },
+    uglify: {
+      options: {
+        mangle: {
+          except: ['ko']
+        }
+      },
+      dist: {
+        files: {
+          'dist/ko-validation.min.js': [ 'dist/ko-validation.js' ]
+        }
+      }
+    },
     watch: {
       tests: {
         files: ['src/**/*.js', 'spec/**/*.js'],
@@ -27,6 +48,9 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', ['karma:all', 'watch']);
+  grunt.registerTask('dist', ['concat:dist', 'uglify:dist']);
 };
