7d6d308cec9a38c98e5ff45424a124c961efdb2a
Luiz Fernando Ribeiro <luizfar@gmail.com>
2013-12-09 16:27:55 -0200

    Fix validators registration.

--- src/validators-registry.js
+++ src/validators-registry.js
@@ -1,9 +1,9 @@
 (function () {
-  ko.validation.registerValidator('required', ko.validators.greaterThanOrEqualToFieldValueValidator);
-  ko.validation.registerValidator('required', ko.validators.invalidCharsValidator);
-  ko.validation.registerValidator('required', ko.validators.lengthValidator);
-  ko.validation.registerValidator('required', ko.validators.lessThanOrEqualToFieldValueValidator);
-  ko.validation.registerValidator('required', ko.validators.rangeValidator);
+  ko.validation.registerValidator('greaterThanOrEqualToFieldValue', ko.validators.greaterThanOrEqualToFieldValueValidator);
+  ko.validation.registerValidator('invalidChars', ko.validators.invalidCharsValidator);
+  ko.validation.registerValidator('length', ko.validators.lengthValidator);
+  ko.validation.registerValidator('lessThanOrEqualToFieldValue', ko.validators.lessThanOrEqualToFieldValueValidator);
+  ko.validation.registerValidator('range', ko.validators.rangeValidator);
   ko.validation.registerValidator('required', ko.validators.requiredValidator);
 }) ();
 
