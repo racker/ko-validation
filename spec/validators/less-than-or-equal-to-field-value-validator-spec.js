17ca5b8fa625e952ec162ab43618a103ec45b23d
JP Bochi <jpbochi@thoughtworks.com>
2013-12-13 11:48:36 -0200

    even numeric validators will receive string values to validate

--- spec/validators/less-than-or-equal-to-field-value-validator-spec.js
+++ spec/validators/less-than-or-equal-to-field-value-validator-spec.js
@@ -14,25 +14,25 @@ describe('ko.validators.lessThanOrEqualToFieldValueValidator', function () {
   });
 
   it('passes if the value is equal to the value in the specified field', function () {
-    result = validator.validate(10);
+    result = validator.validate('10');
 
     expect(result.isValid).toBe(true);
   });
 
   it('passes if the value is less than the value in the specified field', function () {
-    result = validator.validate(9);
+    result = validator.validate('9');
 
     expect(result.isValid).toBe(true);
   });
 
   it('fails if the value is greater than the value in the specified field', function () {
-    result = validator.validate(11);
+    result = validator.validate('11');
 
     expect(result.isValid).toBe(false);
   });
 
   it('shows an error message on failure', function () {
-    result = validator.validate(11);
+    result = validator.validate('11');
 
     expect(result.message).toBe('FieldName must be less than or equal to OtherFieldName');
   });
