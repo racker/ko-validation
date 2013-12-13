1fc41dedb53dc5c5804779c75480a3cb9de11c22
JP Bochi <jpbochi@thoughtworks.com>
2013-12-13 17:37:43 -0200

    fixed regex validator to use utils.validateWithMessage

--- src/validators/regex-validator.js
+++ src/validators/regex-validator.js
@@ -1,14 +1,17 @@
 ko.validators.regexValidator = function (regex, fieldName, messageFragment) {
+  var utils = ko.validators.utilities;
   messageFragment = messageFragment || ko.validators.utilities.buildString(
     'does not match {$regex}',
     { 'regex': regex }
   );
 
   return ko.validators.customValidator(
-    regex.test.bind(regex),
-    ko.validators.utilities.buildString(
-      '{$field} {$messageFragment}.',
-      { 'field': fieldName, 'messageFragment': messageFragment }
+    utils.validateWithMessage(
+      regex.test.bind(regex),
+      utils.buildString(
+        '{$field} {$messageFragment}.',
+        { 'field': fieldName, 'messageFragment': messageFragment }
+      )
     )
   );
 };
