9cf0f787a179292ab543dee9288a4352902a4a33
JP Bochi <jpbochi@thoughtworks.com>
2013-12-17 14:06:29 -0200

    integration test for only_if validator

--- spec/integration-spec.js
+++ spec/integration-spec.js
@@ -6,13 +6,18 @@ describe('ko validation integration', function () {
       '<div id="parent">' +
       '<input id="required-input" data-bind="value: requiredField"/>' +
       '<input id="greater-than-input" data-bind="value: greaterThanField"/>' +
+      '<input id="sometimes-required-input" data-bind="value: sometimesRequired"/>' +
       '</div>'
     );
 
     viewModel = {
       requiredField: ko.observable('').extend({ 'required': ['First Name'] }),
-      greaterThanField: ko.observable('').extend({ 'greaterThan': [ 'greaterThanField', 42 ] })
+      greaterThanField: ko.observable('').extend({ 'greaterThan': [ 'greaterThanField', 42 ] }),
+      isItRequired: ko.observable(true)
     };
+    viewModel['sometimesRequired'] = ko.observable('').extend({
+      'onlyIf': [viewModel.isItRequired, { 'required': [ 'Sometimes' ] }]
+    });
 
     ko.applyBindings(viewModel, $('#parent')[0]);
   });
@@ -42,6 +47,29 @@ describe('ko validation integration', function () {
       $('#greater-than-input').val(43).trigger('change');
 
       expect(viewModel.greaterThanField).toBeValid();
+    });
+  });
+
+  describe('for onlyIf required validator', function () {
+    it('is invalid when requirement is true and value is not valid', function () {
+      viewModel.isItRequired(true);
+      $('#required-input').val('').trigger('change');
+
+      expect(viewModel.requiredField).not.toBeValid();
+    });
+
+    it('is valid', function () {
+      viewModel.isItRequired(true);
+      $('#required-input').val('Jonh').trigger('change');
+
+      expect(viewModel.requiredField).toBeValid();
+    });
+
+    it('is valid when requirement is not true', function () {
+      viewModel.isItRequired(false);
+      $('#required-input').val('').trigger('change');
+
+      expect(viewModel.requiredField).not.toBeValid();
     });
   });
 });
