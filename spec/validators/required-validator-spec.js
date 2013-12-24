5c430d89e592e96985b22e6512f024dd57c21e7c
JP Bochi <jpbochi@thoughtworks.com>
2013-12-24 12:03:59 -0200

    refactored required validator specs; and added test for '0'

--- spec/validators/required-validator-spec.js
+++ spec/validators/required-validator-spec.js
@@ -5,48 +5,49 @@ describe('ko.validators.requiredValidator', function () {
     validator = ko.validators.requiredValidator('Field Name');
   });
 
-  it('fails for empty string', function () {
+  it('is invalid for empty string', function () {
     result = validator.validate('');
     expect(result.isValid).toBe(false);
     expect(result.message).toBe('Field Name is required.');
   });
 
-  it('fails if null', function() {
+  it('is invalid if null', function() {
     result = validator.validate(null);
     expect(result.isValid).toBe(false);
     expect(result.message).toBe('Field Name is required.');
   });
 
-  it('fails if undefined', function() {
+  it('is invalid if undefined', function() {
     result = validator.validate(undefined);
     expect(result.isValid).toBe(false);
     expect(result.message).toBe('Field Name is required.');
   });
 
-  it('fails if false', function() {
+  it('is invalid if false', function() {
     result = validator.validate(false);
     expect(result.isValid).toBe(false);
     expect(result.message).toBe('Field Name is required.');
   });
 
-  it('fails if empty array', function() {
+  it('is invalid if empty array', function() {
     result = validator.validate([]);
     expect(result.isValid).toBe(false);
     expect(result.message).toBe('Field Name is required.');
   });
 
-  it('should not fail if string "false"', function () {
+  it('is valid if string "0"', function () {
+    result = validator.validate('0');
+    expect(result.isValid).toBe(true);
+    expect(result.message).toBe('');
+  });
+
+  it('is valid if string "false"', function () {
     result = validator.validate('false');
     expect(result.isValid).toBe(true);
     expect(result.message).toBe('');
   });
 
-  it('should pass if not empty', function() {
-    result = validator.validate('value');
-    expect(result.isValid).toBe(true);
-  });
-
-  it('should provide an empty message if valid', function() {
+  it('is valid if not empty', function() {
     result = validator.validate('value');
     expect(result.message).toBe('');
   });
