6ee227ea27799ac945401ec32d5cb0131804ea6d
Dave King <dave.king@mailtrust.com>
2013-12-31 16:16:20 -0500

    Update regex validator interfaces to match other validators
    * Human-readable message goes first for all other validators
      (at least the ones I checked)
    * Removed custom field logic because this is not a proper use
      of internationalization.

--- spec/integration-spec.js
+++ spec/integration-spec.js
@@ -61,7 +61,7 @@ describe('ko validation integration', function () {
         'range': [ 'Ranged', 10, 100 ]
       }),
       regexField: ko.observable('').extend({
-        'regex': [ /^[0-9]+$/, 'Regular' ]
+        'regex': [ 'Regular', /^[0-9]+$/ ]
       }),
       customField: ko.observable('').extend({
         'custom': [customValidatorFunction, customValidatorContext]
