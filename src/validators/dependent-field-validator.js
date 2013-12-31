10652f599a224a9db0f1a72691ae0a62252f05d5
JP Bochi <jpbochi@thoughtworks.com>
2013-12-31 13:30:44 -0200

    removed dependent field validator as it is not used anymore

--- src/validators/dependent-field-validator.js
+++
@@ -1,7 +0,0 @@
-ko.validators.dependentFieldValidator = function (otherFieldId, message, comparator) {
-  var delayedOtherFieldValue = ko.validators.utilities.delayedValueByElementId(otherFieldId);
-
-  return ko.validators.compareWithDelayedValueValidator(
-    delayedOtherFieldValue, comparator, message
-  );
-};
