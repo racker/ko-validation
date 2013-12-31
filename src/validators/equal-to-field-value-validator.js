3857b15809bfa8644e2f8f01b2d87b4cfa178842
JP Bochi <jpbochi@thoughtworks.com>
2013-12-31 13:29:59 -0200

    made equalToFieldValueValidator use customValidator directly

--- src/validators/equal-to-field-value-validator.js
+++ src/validators/equal-to-field-value-validator.js
@@ -1,12 +1,17 @@
 ko.validators.equalToFieldValueValidator = function (fieldName, otherFieldName, otherFieldId) {
-  return ko.validators.dependentFieldValidator(
-    otherFieldId,
-    ko.validators.utilities.buildString(
-      '{$field} must be equal to {$otherField}.',
-      { 'field': fieldName, 'otherField': otherFieldName }
-    ),
-    function (value, otherFieldValue) {
-      return value === otherFieldValue;
-    }
+  var utils, delayedOtherFieldValue;
+  utils = ko.validators.utilities;
+  delayedOtherFieldValue = utils.delayedValueByElementId(otherFieldId);
+
+  return ko.validators.customValidator(
+    utils.validateWithMessage(
+      function (value) {
+        return value === delayedOtherFieldValue();
+      },
+      utils.buildString(
+        '{$field} must be equal to {$otherField}.',
+        { 'field': fieldName, 'otherField': otherFieldName }
+      )
+    )
   );
 };
