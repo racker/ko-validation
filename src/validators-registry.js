4edbb08181f2adc1f25d3e7086990386217efb05
willcumbie <will.cumbie@mailtrust.com>
2014-01-03 17:02:21 -0500

    Added length range validator and corresponding tests

--- src/validators-registry.js
+++ src/validators-registry.js
@@ -4,6 +4,7 @@
   ko.validation.registerValidator('invalidChars', ko.validators.invalidCharsValidator);
   ko.validation.registerValidator('length', ko.validators.lengthValidator);
   ko.validation.registerValidator('minLength', ko.validators.minLengthValidator);
+  ko.validation.registerValidator('lengthRange', ko.validators.lengthRangeValidator);
   ko.validation.registerValidator('lessThanOrEqualToFieldValue', ko.validators.lessThanOrEqualToFieldValueValidator);
   ko.validation.registerValidator('range', ko.validators.rangeValidator);
   ko.validation.registerValidator('required', ko.validators.requiredValidator);
