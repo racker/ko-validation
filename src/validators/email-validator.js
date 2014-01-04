51bef55cad903061c5d6297137c59ce2efc0759e
Jigar Solanki <jigar234@gmail.com>
2014-01-04 14:42:43 -0500

    D-16638: Remove unused utils var

--- src/validators/email-validator.js
+++ src/validators/email-validator.js
@@ -1,8 +1,4 @@
 ko.validators.emailValidator = function () {
-  var utils;
-
-  utils = ko.validators.utilities;
-
   return ko.validators.regexValidator(
     /^[+a-zA-Z0-9_.-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,6}$/,
     'Email address',
