3870d4a8a36a27bc766903644b0d766fde7e2c09
Bruno Trecenti <btrecent@thoughtworks.com>
2014-01-21 17:30:56 -0200

    linted view-model-spec.js

--- spec/view-model-spec.js
+++ spec/view-model-spec.js
@@ -2,22 +2,24 @@ describe('View Model with validatable observables', function () {
   var viewModel;
 
   beforeEach(function () {
+    var html;
+
     viewModel = {
       'name': ko.observable().extend({ 'required': ['Name is required.'] }),
       'age': ko.observable().extend({ 'required': ['Age is required.'] })
     };
 
-    setFixtures(
-      '<div id="parent">' +
-        '<input id="name" data-bind="value: name"/>' +
-        '<input id="age" data-bind="value: age"/>' +
-      '</div>'
-    );
+    html = '<div id="parent">' +
+      '<input id="name" data-bind="value: name"/>' +
+      '<input id="age" data-bind="value: age"/>' +
+      '</div>';
+
+    setFixtures(html);
     ko.applyBindings(viewModel, $('#parent')[0]);
   });
 
   it('has a "validate" method added to it', function () {
-    expect(typeof(viewModel.validate)).toBe('function');
+    expect(typeof viewModel.validate).toBe('function');
   });
 
   it('validates all its observables when "validate" is called', function () {
