cb7f1b6297314776985c3c60a1799203592cb53d
Bruno Trecenti <btrecent@thoughtworks.com>
2014-01-21 17:26:49 -0200

    linted function(

--- spec/validators/invalid-chars-validator-spec.js
+++ spec/validators/invalid-chars-validator-spec.js
@@ -1,29 +1,29 @@
 describe('ko.validators.invalidCharsValidator', function () {
   var validator;
 
-  beforeEach(function() {
+  beforeEach(function () {
     validator = ko.validators.invalidCharsValidator(['a', 'b'], "Must not contain 'a' nor 'b'.");
   });
 
-  it('passes when the value does not contain invalid chars', function() {
+  it('passes when the value does not contain invalid chars', function () {
     expect(validator.validate('foo').isValid).toBe(true);
   });
 
-  it('fails when the value does contain invalid chars', function() {
+  it('fails when the value does contain invalid chars', function () {
     expect(validator.validate('foao').isValid).toBe(false);
   });
 
-  it('passes with an empty string', function() {
+  it('passes with an empty string', function () {
     expect(validator.validate('')).toEqual({ isValid: true });
   });
 
-  it('works with spaces', function() {
+  it('works with spaces', function () {
     validator = ko.validators.invalidCharsValidator([' ']);
     expect(validator.validate('fo o').isValid).toBe(false);
     expect(validator.validate('foo').isValid).toBe(true);
   });
 
-  it('has correct message on failure', function() {
+  it('has correct message on failure', function () {
     expect(validator.validate('fooa')).toEqual({
       isValid: false,
       message: "Must not contain 'a' nor 'b'."
--- spec/validators/range-validator-spec.js
+++ spec/validators/range-validator-spec.js
@@ -1,46 +1,46 @@
-describe('ko.validators.rangeValidator', function() {
+describe('ko.validators.rangeValidator', function () {
   var validator, result;
 
-  beforeEach(function() {
+  beforeEach(function () {
     validator = ko.validators.rangeValidator(1, 10, 'Must be between 1 and 10.');
   });
 
-  it('should pass if value is a number within range', function() {
+  it('should pass if value is a number within range', function () {
     result = validator.validate(5);
     expect(result.isValid).toBe(true);
   });
 
-  it('should pass if value is a string within range', function() {
+  it('should pass if value is a string within range', function () {
     result = validator.validate('5');
     expect(result.isValid).toBe(true);
   });
 
-  it('should fail if not a number', function() {
+  it('should fail if not a number', function () {
     result = validator.validate('10a');
     expect(result.isValid).toBe(false);
   });
 
-  it('should fail if not an integer', function() {
+  it('should fail if not an integer', function () {
     result = validator.validate('9.5');
     expect(result.isValid).toBe(false);
   });
 
-  it('should fail if less than range', function() {
+  it('should fail if less than range', function () {
     result = validator.validate(0);
     expect(result.isValid).toBe(false);
   });
 
-  it('should fail if greater than range', function() {
+  it('should fail if greater than range', function () {
     result = validator.validate(11);
     expect(result.isValid).toBe(false);
   });
 
-  it('should not provide error message when valid', function() {
+  it('should not provide error message when valid', function () {
     result = validator.validate(10);
     expect(result).toEqual({ isValid: true });
   });
 
-  it('should provide a meaningful error message when not valid', function() {
+  it('should provide a meaningful error message when not valid', function () {
     result = validator.validate(0);
     expect(result).toEqual({
       isValid: false,
