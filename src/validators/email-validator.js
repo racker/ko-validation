2a64c788c2352218ba88ca00bb6b5184d02aff25
tdruiva <tsilva@thoughtworks.com>
2014-03-24 11:33:37 -0300

    Fixed, need comma to match with different than 2

--- src/validators/email-validator.js
+++ src/validators/email-validator.js
@@ -1,6 +1,6 @@
 ko.validators.emailValidator = function (message) {
   return ko.validators.regexValidator(
-    /^[+a-zA-Z0-9_.\-]+@([a-zA-Z0-9\-]+\.)+[a-zA-Z0-9]{2}$/,
+    /^[+a-zA-Z0-9_.\-]+@([a-zA-Z0-9\-]+\.)+[a-zA-Z0-9]{2,}$/,
     message
   );
 };
